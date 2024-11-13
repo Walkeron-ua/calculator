6dbaa870c33b38cdb872fb7ea19c70d70a813cfe
Walkeron-ua <177946022+Walkeron-ua@users.noreply.github.com>
2024-11-13 16:45:06 +0200

    Create operate function to process calculation
    -Implemented the operate function, which takes three arguments:
     firstNumber, secondNumber, and operator, and calls the appropriate
     arithmetic function (add, subtract, multiply, or divide)
     based on the operator provided.
    -Added a temporary do-while loop to prompt the user for input
     (two numbers and an operator) and pass the values
     to the operate function.
    -The result is displayed in an alert, and the user is prompted
     whether they want to continue the calculation or exit the loop.

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(firstN, secondN, operator) {
+    if (operator === "+") return add(firstN, secondN);
+    if (operator === "-") return subtract(firstN, secondN);
+    if (operator === "*") return multiply(firstN, secondN);
+    if (operator === "/") {
+        if (secondN === 0) return alert("Can't divide by 0");
+        return divide(firstN, secondN);
+    } 
+}
+
+let check;
+do {
+    let firstNumber = +prompt("Enter first number");
+    let secondNumber = +prompt("Enter second number");
+    let operator = prompt("Enter operator you want to do ('+', '-', '*', or '/')");
+    alert(operate(firstNumber, secondNumber, operator));
+    check = prompt("Press 'OK' to continue otherwise 'Close'")
+} while (check !== null);
